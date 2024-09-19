f46f88bb98e485f59d478b7115775ea138dbe07c
Dosso <massidedosso@gmail.com>
2024-09-19 18:51:08 +0200

    Initial Commit

---
+++ myArt/myArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  myArtApp.swift
+//  myArt
+//
+//  Created by Massidé Dosso on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct myArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
