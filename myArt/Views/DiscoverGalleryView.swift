2b8d69a2a997b0103b316137f8d129f3185e9f01
“diakhite-sio” <diakhite.ibrahima.sio@gmail.com>
2024-10-23 15:06:30 +0200

    Add Tab Bar

--- myArt/Views/DiscoverGalleryView.swift
+++ myArt/Views/DiscoverGalleryView.swift
@@ -26,7 +26,7 @@ struct  DiscoverMuseumView: View {
             
             NavigationStack
             {
-                
+             
                 ScrollView
                 {
                     
