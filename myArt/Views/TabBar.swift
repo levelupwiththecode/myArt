9ee098b18d340ac44ab8bcf0e0df194c16462583
“diakhite-sio” <diakhite.ibrahima.sio@gmail.com>
2024-10-24 11:06:18 +0200

    push All Branch  FinalIbrahimaDev

---
+++ myArt/Views/TabBar.swift
@@ -0,0 +1,18 @@
+//
+//  TabBar.swift
+//  myArt
+//
+//  Created by Apprenant 163 on 24/10/2024.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBar()
+}
